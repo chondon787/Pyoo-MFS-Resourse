565ef5ceab2b051750d7f17ddbddcd05d0a19ce9
Chondon Roy <chondonroy787@gmail.com>
2024-09-19 19:18:20 +0600

    my second commit

--- js/login.js
+++ js/login.js
@@ -1,11 +1,37 @@
 // step number...........1
+/*
 
 document.getElementById('button-login').addEventListener('click',function(event){
     // step number........2 prevent loadding browser
     event.preventDefault();
     console.log('wellcome to click me ')
 
-    // step number...........3
+    // step-3 numberand pin..........3
     const phoneNumber = document.getElementById('phone-number').value;
-    console.log(phoneNumber);
+    const pinNumber = document.getElementById('pin-number').value;
+    console.log(phoneNumber, pinNumber);
+
+    // Validet number and pin
+    if(phoneNumber === '5' && pinNumber ==='1234'){
+        console.log('you are logged in')
+    }
+    else{
+        alert('invalid number or pin')
+    }
 })
+*/
+
+document.getElementById('button-login').addEventListener('click',function(event){
+    event.preventDefault();
+    const phoneNumber = document.getElementById('phone-number').value
+    const pinNumber = document.getElementById('pin-number').value
+    // console.log(phoneNumber, pinNumber)
+
+    if(phoneNumber ==='5' && pinNumber === '5555'){
+        console.log('you are logged in sucsessful');
+       window.location.href = 'home.html'
+        }
+        else{
+            alert('wrong phonnumber or number')
+    }
+});
