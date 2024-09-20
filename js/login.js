355dcbb1b2c10d80c160041d67554f9b472a085e
Chondon Roy <chondonroy787@gmail.com>
2024-09-20 23:10:59 +0600

    again added

--- js/login.js
+++ js/login.js
@@ -1,37 +1,17 @@
-// step number...........1
-/*
+
+// creat login 
 
 document.getElementById('button-login').addEventListener('click',function(event){
-    // step number........2 prevent loadding browser
     event.preventDefault();
-    console.log('wellcome to click me ')
+    const phoneNumber = document.getElementById('phone-number').value
+    const pinNumber = document.getElementById('pin-number').value
+    // console.log(phoneNumber, pinNumber)
 
-    // step-3 numberand pin..........3
-    const phoneNumber = document.getElementById('phone-number').value;
-    const pinNumber = document.getElementById('pin-number').value;
-    console.log(phoneNumber, pinNumber);
-
-    // Validet number and pin
-    if(phoneNumber === '5' && pinNumber ==='1234'){
-        console.log('you are logged in')
+    if(phoneNumber ==='01302220150' && pinNumber === '5555'){
+        console.log('you are logged in sucsessful');
+       window.location.href = 'home.html'
+        }
+        else{
+            alert('wrong phonnumber or number')
     }
-    else{
-        alert('invalid number or pin')
-    }
-})
-*/
-
-// document.getElementById('button-login').addEventListener('click',function(event){
-//     event.preventDefault();
-//     const phoneNumber = document.getElementById('phone-number').value
-//     const pinNumber = document.getElementById('pin-number').value
-//     // console.log(phoneNumber, pinNumber)
-
-//     if(phoneNumber ==='01302220150' && pinNumber === '5555'){
-//         console.log('you are logged in sucsessful');
-//        window.location.href = 'home.html'
-//         }
-//         else{
-//             alert('wrong phonnumber or number')
-//     }
-// });
+});
