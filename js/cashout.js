336c69f1aafb14446f7beb6a67cf16d1f8c023d6
Chondon Roy <chondonroy787@gmail.com>
2024-09-20 23:05:16 +0600

    finnal and cheak

--- js/cashout.js
+++ js/cashout.js
@@ -1,53 +1,32 @@
-// // cashout section start
-
-// document.getElementById('btn-cash-out-money').addEventListener('click',function(event){
-//     event.preventDefault();
-//     // cashout money aand send money
-//     const cashOutMoney = document.getElementById('input-cash-out-money').value;
-//     const cashOut = parseFloat(cashOutMoney)
-//     const PinNumber = document.getElementById('input-cash-out-pin-number').value;
-
-    
-// //  reduce the mouney
-// const newMoney = balanceNumber - cashOut
-     
-
-//     if(PinNumber === '5555'){
-        
-//         const balance = document.getElementById('account-balance').innerText;
-//      const balanceNumber = parseFloat(balance)
-//      const balance = document.getElementById('account-balance').innerText = newMoney;
-
-//     }
-//     else{
-//         alert('faild plz try letter')
-//     }
-        
-// })
-
-
-
-
-document.getElementById('btn-cash-out-money').addEventListener('click', function(event) {
+// step-1 ......cash out btn money
+document.getElementById('btn-cash-out').addEventListener('click',function(event){
     event.preventDefault();
 
+    // step-2...const or variable cash out money and pin
     const cashOutMoney = document.getElementById('input-cash-out-money').value;
-    const cashOut = parseFloat(cashOutMoney);
-    const PinNumber = document.getElementById('input-cash-out-pin-number').value;
+    const cashOutPin = document.getElementById('input-cash-out-pin-number').value;
 
-     if (PinNumber === '5555') {
-        
-        const balance = document.getElementById('account-balance').innerText;
-        const balanceNumber = parseFloat(balance);
-
-         
-        if (balanceNumber >= cashOut) {
-            const newMoney = balanceNumber - cashOut;
-            document.getElementById('account-balance').innerText = newMoney;
-        } else {
-            alert('successfull');
-        }
-    } else {
-        alert('fild plz try latter');
+    // step-3.... pin code set
+    if(cashOutPin === '5555'){
+        console.log('transition successfull')
     }
-});
+    else{
+        alert('Faild plz try again')
+    }
+
+    // step-4..main= balance stand a variable called accountBalance
+    const Balance = document.getElementById('account-balance').innerText;
+
+    // step-5 chang the string by const a number
+    const cashOutNumber = parseFloat(cashOutMoney);
+    const BalanceNumber = parseFloat(Balance);
+    const newNumber = BalanceNumber - cashOutNumber
+    
+    // step-6 newnumber reduc (-) main balance 
+    document.getElementById('account-balance').innerText = newNumber;
+})
+
+
+
+
+ 
--- js/login.js
+++ js/login.js
@@ -21,17 +21,17 @@ document.getElementById('button-login').addEventListener('click',function(event)
 })
 */
 
-document.getElementById('button-login').addEventListener('click',function(event){
-    event.preventDefault();
-    const phoneNumber = document.getElementById('phone-number').value
-    const pinNumber = document.getElementById('pin-number').value
-    // console.log(phoneNumber, pinNumber)
+// document.getElementById('button-login').addEventListener('click',function(event){
+//     event.preventDefault();
+//     const phoneNumber = document.getElementById('phone-number').value
+//     const pinNumber = document.getElementById('pin-number').value
+//     // console.log(phoneNumber, pinNumber)
 
-    if(phoneNumber ==='01302220150' && pinNumber === '5555'){
-        console.log('you are logged in sucsessful');
-       window.location.href = 'home.html'
-        }
-        else{
-            alert('wrong phonnumber or number')
-    }
-});
+//     if(phoneNumber ==='01302220150' && pinNumber === '5555'){
+//         console.log('you are logged in sucsessful');
+//        window.location.href = 'home.html'
+//         }
+//         else{
+//             alert('wrong phonnumber or number')
+//     }
+// });
